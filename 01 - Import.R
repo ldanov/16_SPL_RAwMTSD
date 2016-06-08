60bfd0b2a8b9f8f22f74e52fa22ae25cb9eca324
ldanov <lubomir.danov@gmail.com>
2016-06-09 00:10:38 +0200

    1.1

--- 01 - Import.R
+++ 01 - Import.R
@@ -19,9 +19,13 @@ if (isTRUE(all.equal(input$Code1, input$Code2)) &
 }
 
 ### Check if numeric columns are interpreted as numeric, if not convert them
+### Try to convert each column, whose name does not begin with Code (case insensitive),
+### into a numeric one. If successful keep both, but return a vector of all successes;
+### if unsuccessful remove attempt (as it will most likely yield an NA vector)
 colCheckInd <- 1:ncol(input)
 colCheckInd <- colCheckInd[!colCheckInd %in% grep("Code", colnames(input), ignore.case = TRUE)]
 origCols <- 0
+
 for (i in colCheckInd) {
   if (class(input[,i]) != "numeric")
   {
@@ -33,7 +37,8 @@ for (i in colCheckInd) {
            print(paste0("Column ",colnames(input)[i]," converted to numeric in column ", colnames(input)[ncol(input)])))
     temp <- i; origCols <- c(origCols, temp)
   } 
-}; rm (i, colCheckInd, temp); origCols <- origCols[-1]; if(length(origCols)==0) rm(origCols)
+}; rm (i, colCheckInd, temp); origCols <- origCols[-1]
 
+if(length(origCols)==0) {rm(origCols)
+  } else print(paste0("Columns that were attempted to convert: ", toString(origCols)))
 
-#print(paste0("Columns tried to convert: ", toString(origCols)))
