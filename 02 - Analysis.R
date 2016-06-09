4030c85ae58d20f2ad5c2eee26b447551267295d
ldanov <lubomir.danov@gmail.com>
2016-06-09 11:40:24 +0200

    Now with plots

--- 02 - Analysis.R
+++ 02 - Analysis.R
@@ -19,5 +19,23 @@ resultsStationary <- within(resultsStationary, {
   DFstat <- as.numeric(as.character(DFstat))
   pvalue <- as.numeric(as.character(pvalue))
 })
+plot(resultsStationary$lag, resultsStationary$DFstat)
+plot(resultsStationary$lag, resultsStationary$pvalue)
 
+resultsExplosive <- data.frame(matrix(vector(), nrow = 0, ncol = 6))
+for (i in 2:5){
+  for (j in 1:ceiling(.9*nrow(input))) {
+    temp <- adf.test(input[,i], alternative = "e", k=j)
+    temp <- t(unlist(temp))
+    resultsExplosive <- rbind (resultsExplosive, temp) 
+  }
+}
+rm(temp, i ,j)
+colnames(resultsExplosive)<- c("DFstat", "lag", "alternative", "pvalue", "method", "data")
+resultsExplosive <- within(resultsExplosive, {
+  DFstat <- as.numeric(as.character(DFstat))
+  pvalue <- as.numeric(as.character(pvalue))
+})
 
+plot(resultsExplosive$lag, resultsExplosive$DFstat)
+plot(resultsExplosive$lag, resultsExplosive$pvalue)
