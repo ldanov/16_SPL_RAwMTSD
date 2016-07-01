3d185941c1c5a0610f73be3a3831fa72e9590926
ldanov <lubomir.danov@gmail.com>
2016-07-01 12:23:50 +0200

    C

--- source/PrepPackages.R
+++ source/PrepPackages.R
@@ -1,4 +1,4 @@
-### Load packages from vector, if not in installed packages, install.
+### Load packages from vector, if not in installed packages, install.#
 PrepPackages <- function(requiredLibs){
   notInstalled <- requiredLibs[!(requiredLibs %in% installed.packages()[,"Package"])]
   if (length(notInstalled)) install.packages(notInstalled, dependencies = TRUE)
