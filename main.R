7824188f148a8ffd8ae4e0df9fe980031c82f610
ldanov <ldanov@users.noreply.github.com>
2016-08-07 14:17:38 +0200

    Adjusting to Quantlet
    REdoing code structure and formatting to fit SPL and Quantlet
    requirements

--- main.R
+++
@@ -1,8 +0,0 @@
-### Source all functions
-source("00_functions.R")
-source("01_import_data.R")
-timeCols <- c("Year", "timeIndex")
-source("02_transform_data.R")
-source("03_basic_plots.R")
-source("04_test_statistics.R")
-source("05_ts_regressions.R")
--- source/PrepPackages.R
+++
@@ -1,6 +0,0 @@
-### Load packages from vector, if not in installed packages, install.#
-PrepPackages <- function(requiredLibs){
-  notInstalled <- requiredLibs[!(requiredLibs %in% installed.packages()[,"Package"])]
-  if (length(notInstalled)) install.packages(notInstalled, dependencies = TRUE)
-  lapply(requiredLibs, require, character.only=T)
-}
